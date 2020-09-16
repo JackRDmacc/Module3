3d7a77fe5d948f108494fff479a94b8ea8fb7dad
Reser, Jack C <jcreser@dmacc.edu>
2020-09-15 22:53:19 -0500

    updated method in average_scores.  test passes

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,6 +1,10 @@
 
 def average_scores():
-    pass
+    score1 = input("Enter your first score: ")
+    score2 = input("Enter your second score: ")
+    score3 = input("Enter your third score: ")
+
+    return (int(score1) + int(score2) + int(score3)) / 3
 
 if __name__ == '__main__':
     pass
