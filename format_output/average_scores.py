0eb6f7a5ca6e613f284a922d87761fb9d5bd2486
Reser, Jack C <jcreser@dmacc.edu>
2020-09-15 23:07:47 -0500

    updated main method. added docstring & results comment.  test passes

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,5 +1,12 @@
+"""
+Program: average_scores.py
+Author: Jack Reser
 
-def average_scores():
+program that accepts 3 test scores, first and last name, and age.  Prints out info
+"""
+
+
+def average():
     score1 = input("Enter your first score: ")
     score2 = input("Enter your second score: ")
     score3 = input("Enter your third score: ")
@@ -7,4 +14,13 @@ def average_scores():
     return (int(score1) + int(score2) + int(score3)) / 3
 
 if __name__ == '__main__':
-    pass
+    last_name = input("Enter your last name: ")
+    first_name = input("Enter your first name: ")
+    age = input("Enter your age: ")
+    average_scores = average()
+
+    print("{}, {} age: {} average grade: {}".format(last_name, first_name, age, average_scores))
+
+
+#Test scores: (80, 85, 90)  First: Jack  Last: Reser  Age: 22
+# Output: Reser, Jack age: 22 average grade: 85.0
--- test/test_average_scores.py
+++ test/test_average_scores.py
@@ -6,7 +6,7 @@ from format_output import average_scores
 class MyTestCase(unittest.TestCase):
     def test_average(self):
         with mock.patch('builtins.input', side_effect=[85,90,95]):
-            assert average_scores.average_scores() == 90
+            assert average_scores.average() == 90
 
 
 if __name__ == '__main__':
